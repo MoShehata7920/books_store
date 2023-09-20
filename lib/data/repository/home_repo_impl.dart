f3f3f5d25f0516c1f2ca2c0776a704fae6680ca2
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2023-09-21 02:51:02 +0300

    featured books repo

--- lib/data/repository/home_repo_impl.dart
+++ lib/data/repository/home_repo_impl.dart
@@ -33,8 +33,24 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              "volumes?Filtering=free-ebooks &q=subject:programming");
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+
+      return left(ServerFailure.fromDioError(e.toString() as DioException));
+    }
   }
 }
